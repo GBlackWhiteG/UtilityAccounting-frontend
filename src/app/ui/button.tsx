e368702306c27fba4a918755a8735176a8242c6d
Akhmetov Salavat <salavat_akhmetov_2005@mail.ru>
2024-04-21 22:23:50 +0300

    map with two content blocks

---
+++ src/app/ui/button.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import styles from "./button.module.css"
+
+export const Button: FC<{ text: string }> = ({ text }):JSX.Element => {
+    return (
+        <button className={styles.button}>
+            <span className={styles.buttonText}>{text}</span>
+        </button>
+    )
+};
